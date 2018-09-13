83695e126a8e1c53479f7f8b6d855e9119ee795e
angeloLuna <angel.luna.najera@gmail.com>
2018-09-12 21:52:07 -0500

    icons, fonts, background image

---
+++ piedra-papel/main.js
@@ -0,0 +1,14 @@
+let player1 = "piedra"
+let player2 = "papel"
+
+if (player1 === "piedra" && player2 === "papel") {
+	console.log(`El jugador 1 eligio: ${player1}; el jugador 2 eligio: ${player2}, gana jugador 2`);
+	
+}
+
+
+
+
+
+
+
